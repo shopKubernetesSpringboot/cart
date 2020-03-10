50e95de311985216714f666a0ffc04c03379bac2
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-10 04:25:24 +0100

    Gradle

--- src/main/java/com/dgf/shopCart/App.java
+++ src/main/java/com/dgf/shopCart/App.java
@@ -1,25 +1,38 @@
 package com.dgf.shopCart;
 
-import com.dgf.shopCart.service.CartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.session.ReactiveMapSessionRepository;
+import org.springframework.session.config.annotation.web.server.EnableSpringWebSession;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+@Configuration
 @SpringBootApplication
-@Slf4j
+@EnableSpringWebSession
 @EnableWebFlux
-public class App /*implements ApplicationContextAware*/ {
+@Slf4j
+public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-//    @Override
-//    public void setApplicationContext(ApplicationContext ctx) {
-//        ctx.getBean(CartService.class).initData();
-//    }
+    @Bean
+    public ReactiveMapSessionRepository reactiveSessionRepository() {
+        return new ReactiveMapSessionRepository(new ConcurrentHashMap<>());
+    }
 
+    @Bean
+    @Primary
+    public Validator springValidator() {
+        return new LocalValidatorFactoryBean();
+    }
 }
