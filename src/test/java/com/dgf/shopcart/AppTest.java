64674386cefbc7a7502fe8661e9e71c1713b8899
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-10 19:48:52 +0100

    Test Coverage - App

--- src/test/java/com/dgf/shopcart/AppTest.java
+++ src/test/java/com/dgf/shopcart/AppTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@SpringBootTest(classes = App.class)
 public class AppTest {
 
     @Test
