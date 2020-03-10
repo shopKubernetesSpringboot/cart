675f97c0b18ca97e1f53b6420a43a137130240a8
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-10 16:02:56 +0100

    Test coverage

--- src/test/java/com/dgf/shopcart/rest/CartRouterTest.java
+++ src/test/java/com/dgf/shopcart/rest/CartRouterTest.java
@@ -6,7 +6,9 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 public class CartRouterTest {
@@ -17,14 +19,16 @@ public class CartRouterTest {
     @BeforeEach
     public void setUp() {
 
-        RouterFunction<?> route = route(GET("/cart/add"), request ->
-                ServerResponse.ok().bodyValue("It works!"));
+        RouterFunction<?> route = route(PUT("/cart/add").and(accept(APPLICATION_JSON)).and(contentType(APPLICATION_JSON)),
+                request -> ServerResponse.ok().bodyValue("It works!"));
         this.testClient = WebTestClient.bindToRouterFunction(route).build();
     }
 
     @Test
     public void add() {
-        this.testClient.get().uri("/cart/add")
+        this.testClient.put().uri("/cart/add")
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class).isEqualTo("It works!");
