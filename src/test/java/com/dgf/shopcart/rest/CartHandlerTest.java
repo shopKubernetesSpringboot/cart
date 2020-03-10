ec3a567f1a212d2d14c785658526fc40b295ef9a
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-10 18:29:51 +0100

    Test Coverage CartHandler

--- src/test/java/com/dgf/shopcart/rest/CartHandlerTest.java
+++ src/test/java/com/dgf/shopcart/rest/CartHandlerTest.java
@@ -5,6 +5,8 @@ import com.dgf.shopcart.rest.handler.CartHandler;
 import com.dgf.shopcart.rest.handler.req.CartItemAddRequest;
 import com.dgf.shopcart.rest.handler.validation.MyValidator;
 import com.dgf.shopcart.service.CartService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -24,14 +26,17 @@ import org.springframework.web.reactive.result.view.ViewResolver;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 
 @WebFluxTest
-//@RunWith(SpringRunner.class)
 public class CartHandlerTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final HandlerStrategies strategies = HandlerStrategies.withDefaults();
 
     @MockBean
     private CartService service;
@@ -40,12 +45,10 @@ public class CartHandlerTest {
     @Autowired
     private CartHandler handler;
     private ServerResponse.Context context;
-//    private WebTestClient testClient;
 
 
     @BeforeEach
     public void createContext() {
-        HandlerStrategies strategies = HandlerStrategies.withDefaults();
         context = new ServerResponse.Context() {
             @Override
             public List<HttpMessageWriter<?>> messageWriters() {
@@ -59,59 +62,47 @@ public class CartHandlerTest {
         };
     }
 
-
     @Test
-    public void handle() {
-
-//        Gson gson = new Gson();
-
+    public void add() throws JsonProcessingException {
         MockServerWebExchange exchange = MockServerWebExchange.from(
                 MockServerHttpRequest.put("/cart/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
-                        .body("{ \"item\": { \"id\": 1, \"name\": \"product1\" } }"));
-
-        MockServerHttpResponse mockResponse = exchange.getResponse();
-
+                        .body(mapper.writeValueAsString(new Item(1L,"product1"))));
         Mockito.when(validator.validate(any())).thenReturn(Mono.just(new CartItemAddRequest(new Item(1L,"product1"))));
         Mockito.when(service.add(any(),any())).thenReturn(Mono.just(new Item(1L,"product1")));
-
         ServerRequest serverRequest = ServerRequest.create(exchange, HandlerStrategies.withDefaults().messageReaders());
-
         Mono<ServerResponse> serverResponseMono = handler.add(serverRequest);
-
         Mono<Void> voidMono = serverResponseMono.flatMap(response -> {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK);
             return response.writeTo(exchange, context);
         });
-
         StepVerifier.create(voidMono)
                 .expectComplete().verify();
-
+        MockServerHttpResponse mockResponse = exchange.getResponse();
         StepVerifier.create(mockResponse.getBody())
                 .consumeNextWith(System.out::println)
                 .expectComplete().verify();
-
         assertThat(mockResponse.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
-
     }
-
-//    @BeforeEach
-//    public void setUp() {
-//        CartHandler usersHandler = new CartHandler(service, validator);
-//        this.testClient = WebTestClient.bindToController(usersHandler).configureClient().baseUrl("/cart").build();
-//    }
-//
-//    @Test
-//    public void add() {
-//        this.testClient.put().uri("/add")
-//                .accept(APPLICATION_JSON)
-//                .contentType(APPLICATION_JSON)
-//                .bodyValue("{ 'item': { 'id': 1, 'name': 'product1' } }")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectBody()
-//                .json("");
-//    }
+    @Test
+    public void list() {
+        MockServerWebExchange exchange = MockServerWebExchange.from(
+                MockServerHttpRequest.get("/cart/list"));
+        Mockito.when(service.list(any())).thenReturn(Mono.just(new ArrayList<>()));
+        ServerRequest serverRequest = ServerRequest.create(exchange, HandlerStrategies.withDefaults().messageReaders());
+        Mono<ServerResponse> serverResponseMono = handler.list(serverRequest);
+        Mono<Void> voidMono = serverResponseMono.flatMap(response -> {
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK);
+            return response.writeTo(exchange, context);
+        });
+        StepVerifier.create(voidMono)
+                .expectComplete().verify();
+        MockServerHttpResponse mockResponse = exchange.getResponse();
+        StepVerifier.create(mockResponse.getBody())
+                .consumeNextWith(System.out::println)
+                .expectComplete().verify();
+        assertThat(mockResponse.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+    }
 
 }
