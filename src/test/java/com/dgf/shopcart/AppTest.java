4fc309090d9359a9219af75d8adf8163d3f41f11
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-10 20:07:55 +0100

    Test Coverage - lombok @Data mark as generated

--- src/test/java/com/dgf/shopcart/AppTest.java
+++ src/test/java/com/dgf/shopcart/AppTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(classes = App.class)
+@SpringBootTest
 public class AppTest {
 
     @Test
