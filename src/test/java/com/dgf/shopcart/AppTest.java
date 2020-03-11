deb152276e7dfab2ec521ea37c1592a3c48bcd06
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-11 11:46:44 +0100

    GlobalExceptionHandlerTest

--- src/test/java/com/dgf/shopcart/AppTest.java
+++ src/test/java/com/dgf/shopcart/AppTest.java
@@ -1,16 +1,16 @@
 package com.dgf.shopcart;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class AppTest {
 
     @Test
     public void contextLoads() { //todo doesn't cover
-        assertTrue(true);
     }
 
 }
--- src/test/java/com/dgf/shopcart/rest/handler/GlobalExceptionHandlerTest.java
+++ src/test/java/com/dgf/shopcart/rest/handler/GlobalExceptionHandlerTest.java
@@ -7,18 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebInputException;
-import org.springframework.web.server.WebHandler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Import(TestConfig.class)
 @WebFluxTest
@@ -28,27 +26,20 @@ public class GlobalExceptionHandlerTest {
     private CartService cartService;
     @Autowired
     private GlobalExceptionHandler globalExceptionHandler;
+    @Autowired
+    private GlobalErrorAttributes globalErrorAttributes;
 
     @Test
-    public void test() {
-//        RouterFunction<ServerResponse> routingFunction = globalExceptionHandler.getRoutingFunction(new GlobalErrorAttributes());
-//        HandlerFunction<ServerResponse> handlerFunction = request -> Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found"));
-//        RouterFunction<ServerResponse> routerFunction = RouterFunctions.route(RequestPredicates.all(), handlerFunction);
-        GlobalErrorAttributes globalErrorAttributes = new GlobalErrorAttributes();
+    public void badRequest() {
         MockServerHttpRequest httpRequest = MockServerHttpRequest.put("/cart/add")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
                 .build();
-//                .body("{}");
         MockServerWebExchange exchange = MockServerWebExchange.from(httpRequest);
         globalErrorAttributes.storeErrorInformation(new ServerWebInputException("Test error"), exchange);
         RouterFunction<ServerResponse> routerFunction = globalExceptionHandler.getRoutingFunction(globalErrorAttributes);
-
-        WebHandler result = RouterFunctions.toWebHandler(routerFunction);
-        assertNotNull(result);
-
-        result.handle(exchange).block();
-        assertEquals(HttpStatus.BAD_REQUEST,exchange.getResponse().getStatusCode());
+        RouterFunctions.toWebHandler(routerFunction).handle(exchange).block();
+        assertEquals(BAD_REQUEST,exchange.getResponse().getStatusCode());
     }
 
 }
