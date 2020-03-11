9d223c329afb07c2737819feef9f84e0d321ceca
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-11 11:07:53 +0100

    GlobalExceptionHandlerTest

--- src/test/java/com/dgf/shopcart/rest/handler/GlobalExceptionHandlerTest.java
+++ src/test/java/com/dgf/shopcart/rest/handler/GlobalExceptionHandlerTest.java
@@ -7,15 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.http.server.reactive.MockServerHttpResponse;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebInputException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Import(TestConfig.class)
@@ -29,29 +32,27 @@ public class GlobalExceptionHandlerTest {
 
     @Test
     public void test() {
-
 //        RouterFunction<ServerResponse> routingFunction = globalExceptionHandler.getRoutingFunction(new GlobalErrorAttributes());
-
 //        HandlerFunction<ServerResponse> handlerFunction = request -> Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found"));
 //        RouterFunction<ServerResponse> routerFunction = RouterFunctions.route(RequestPredicates.all(), handlerFunction);
         GlobalErrorAttributes globalErrorAttributes = new GlobalErrorAttributes();
-        MockServerWebExchange exchange = MockServerWebExchange.from(
-                MockServerHttpRequest.put("/cart/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .body(""));
-        globalErrorAttributes.storeErrorInformation(new ServerWebInputException(""), exchange);
+        MockServerHttpRequest httpRequest = MockServerHttpRequest.put("/cart/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+//                .body("{}");
+        MockServerWebExchange exchange = MockServerWebExchange.from(httpRequest);
+        globalErrorAttributes.storeErrorInformation(new ServerWebInputException("Test error"), exchange);
         RouterFunction<ServerResponse> routerFunction = globalExceptionHandler.getRoutingFunction(globalErrorAttributes);
 
         HttpHandler result = RouterFunctions.toHttpHandler(routerFunction);
         assertNotNull(result);
 
-//        MockServerHttpRequest httpRequest = MockServerHttpRequest.get("https://localhost").build();
-//        MockServerHttpResponse httpResponse = new MockServerHttpResponse();
-////        ERROR_ATTRIBUTE
-////        httpResponse.
-//        result.handle(httpRequest, httpResponse).block();
+        MockServerHttpResponse httpResponse = new MockServerHttpResponse();
+        httpResponse.setComplete();
+        result.handle(httpRequest, httpResponse).block();
 //        assertEquals(HttpStatus.BAD_REQUEST,httpResponse.getStatusCode());
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,httpResponse.getStatusCode());
     }
 
 }
